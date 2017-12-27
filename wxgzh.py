9fbabade8da9634d7d54b7bd7fa3a798d468ce20
ChenWang91 <429445456@qq.com>
2017-12-27 21:51:58 +0800

    Get cookies use selenium
    Signed-off-by: ChenWang91 <429445456@qq.com>

--- wxgzh.py
+++ wxgzh.py
@@ -2,16 +2,26 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 import time
+import json
 def log_in():
-    account = raw_input("Please input account:")
-    password = raw_input("Please input password:")
+    js = {}
+    account = input("Please input account:")
+    password = input("Please input password:")
     web = webdriver.Chrome()
     web.get("https://mp.weixin.qq.com/")
     web.find_element_by_xpath('//*[@id="header"]/div[2]/div/div/form/div[1]/div[1]/div/span/input').send_keys(account)
     time.sleep(3)
     web.find_element_by_xpath('//*[@id="header"]/div[2]/div/div/form/div[1]/div[2]/div/span/input').send_keys(password)
     time.sleep(3)
+    web.find_element_by_xpath('//*[@id="header"]/div[2]/div/div/form/div[3]/label/i').click()
+    time.sleep(3)
     web.find_element_by_xpath('//*[@id="header"]/div[2]/div/div/form/div[4]/a').click()
-
+    time.sleep(15)
+    web.get("https://mp.weixin.qq.com/")
+    cookies =  web.get_cookies()
+    for cookie in cookies:
+        js[cookie['name']] = cookie['value']
+    str_cookies = json.dumps(js)
+    print (str_cookies)
 if __name__ == '__main__':
     log_in()
